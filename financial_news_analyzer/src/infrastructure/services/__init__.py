65f6f99b17024000795a2f9f5b298ca147303928
Enes OR <332461013@ogr.uludag.edu.tr>
2025-08-06 16:51:36 +0300

    Update __init__.py

--- financial_news_analyzer/src/infrastructure/services/__init__.py
+++ financial_news_analyzer/src/infrastructure/services/__init__.py
@@ -5,10 +5,12 @@ Contains all external service implementations
 
 from .mock_news_service import mock_news_service, MockNewsService
 from .mock_market_service import mock_market_service, MockMarketService
+from .feedback_service import FeedbackService
 
 __all__ = [
     'mock_news_service',
     'MockNewsService',
     'mock_market_service',
-    'MockMarketService'
+    'MockMarketService',
+    'FeedbackService'
 ]
